253e71f559d650350d6835907018e5eb8dfe4fca
Matthew Currie <mcurrie@hac.hq.complexminds.com>
2015-12-18 21:26:07 +0000

    add io simulator to test and demo distiioclient functionality

---
+++ iosim.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+# iosim.py
+# Distributed IO System
+# Matthew Currie <matthew@ve7mjc.com> December 2015
+#
+# Simple IO adapter simulator
+# subclass DistIoClient and re-implement a number of
+# relevent methods to demonstrate a simple client
+# subclass
+#
+# Requires iosim.json configuration or provide path
+# to configuration via commandline argument
+
+import distioclient
+
+class IoSim(distioclient.DistIoClient):
+	
+	def init(self):
+		self.num_dio_inputs = 8
+		self.num_dio_outputs = 8
+	
+	def setDigitalOutput(self, channel, value):
+		print("set_dio_output({0},{1})".format(channel, value))
+	
+	def pollInputs(self):
+		# return nothing, which is fine
+		# todo, add random bit flipping to create real
+		# events to work with
+		pass
+			
+sim = IoSim()
